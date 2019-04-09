d18ca576ff2516b976ced2dba9e1148dba562aaf
Takuya K <32584690+sentakumono@users.noreply.github.com>
2019-04-09 13:29:31 -0400

    Add files via upload

--- GoAT/src/GUI.java
+++ GoAT/src/GUI.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 
 public class GUI extends JFrame implements ActionListener, MouseListener, MouseMotionListener{
 
-	private static JPanel gameWindow, board, commentBox, sidebar,timelineBox, boardMargin, pieceLayer;
+	private static JPanel gameWindow, board, commentBox, sidebar,timelineBox, boardMargin;
 	private static JPanel[][] intersections = new JPanel[18][18];
 	private static GridBagConstraints gbc;
 	private static JTextArea comment,timeline;
 	private static JScrollPane scroll1,scroll2;
-	
+	private static ImageIcon black;
 	public static void main(String[] args) {
 		new GUI();
 	}
@@ -26,24 +26,23 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 		gameWindow = new JPanel();
 		gameWindow.setLayout(null);
 		board = new JPanel(new GridLayout(18,18,0,0));
-		board.setPreferredSize(new Dimension(773, 773));
-		board.setBounds(50,50,773,773);
+		board.setPreferredSize(new Dimension(774, 774));
+		board.setBounds(50,50,774,774);
 		board.setVisible(true);
 		board.setEnabled(true);
 		board.addMouseListener(this);
 		board.setBackground(new Color(181,129,32));
 		
-		pieceLayer = new JPanel();
-		pieceLayer.setBounds(50, 50,  773, 773);
-		pieceLayer.setVisible(true);
+		
+		black = new ImageIcon("PieceIcons/GoStoneBlack.png");
+		
 		for(int i = 0; i < 18; i++) {
 			for(int j = 0; j < 18; j++) {
 				intersections[i][j] = new JPanel();
 				intersections[i][j].setPreferredSize(new Dimension(42,42));
 				intersections[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
 				intersections[i][j].setVisible(true);
-				//intersections[i][j].addMouseListener(this);
-					
+				//intersections[i][j].add(new JLabel(black));
 				intersections[i][j].setBackground(new Color(181, 129, 32));
 				
 				board.add(intersections[i][j]);
@@ -52,10 +51,8 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 		boardMargin = new JPanel();
 		boardMargin.setBounds(50, 50, 850, 850);
 		boardMargin.setBackground(new Color(181, 129, 32));
-		boardMargin.setVisible(true);
-		boardMargin.setEnabled(true);
 		boardMargin.setPreferredSize(new Dimension(850, 850));
-		//addConstraint(boardMargin, board, 18, 18, 18, 18, GridBagConstraints.BOTH, GridBagConstraints.PAGE_END);
+		//addConstraint(boardMargin, board, 1, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
 		
 		
 
@@ -97,7 +94,7 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 		scroll2= new JScrollPane(timeline);
 		
 		scroll2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scroll2.setVisible(true);
+		scroll2.setVisible(true);s
 		timelineBox.add(scroll2);
 		
 
@@ -107,16 +104,13 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 		c.gridx = 0;
 		c.gridy = 1;
 		sidebar.add(timelineBox,c);
-		gameWindow.add(board);
-		sidebar.setVisible(true);
 		
+		sidebar.setVisible(true);
+		gameWindow.add(board);
 		//gameWindow.add(boardMargin);
 		gameWindow.add(sidebar);
-		gameWindow.add(board);
-		gameWindow.add(pieceLayer);
 		add(gameWindow);
 		setVisible(true);
-		System.out.println(intersections[1][1].getSize());
 	}
 
 	public void addConstraint(JPanel p, Component c, int x, int y, int w, int h, int f, int a) {
@@ -135,41 +129,36 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public void mouseClicked(MouseEvent e) {
-		printQuadrant(e);
+		printCoordinate(e);
 		placePiece(e);
 	}
 
-	@Override
+
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
+
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+
 		
 	}
 
-	@Override
+
 	public void mousePressed(MouseEvent e) {
 		//printQuadrant(e);
 	}
 
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+	public void mouseReleased(MouseEvent e) {
+
 		
 	}
 
@@ -180,50 +169,19 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	}
 	
 	void placePiece(MouseEvent e) {
-		JLabel piece = new JLabel();
+		int x = (int)Math.floor((double)e.getX()/43);
+		int y = (int)Math.floor((double)e.getY()/43);
+		System.out.println(x + "," + y);
 		
-		Component c = board.getComponentAt(new Point(e.getX(), e.getY()));
-		int mouseIndexX = ((int)Math.floor(e.getX()) + 17)/ c.getSize().width;
-		int mouseIndexY = ((int)Math.floor(e.getY()) + 17)/ c.getSize().height;
-		
-		if(mouseIndexX < 18 ||mouseIndexY < 18) {
-			
-			piece.setIcon(new ImageIcon("PieceIcons/GoStoneBlack.png"));
-			piece.setLocation(c.getLocation().x, c.getLocation().y);
-		}
-		else{
-			if(mouseIndexX >= 18 && mouseIndexY <18){
-				
-			}
-			if(mouseIndexX < 18 && mouseIndexY >=18){
-				
-			}
-			if(mouseIndexX >= 18 && mouseIndexY >=18){
-				
-			}
-		}
-		
-		pieceLayer.add(piece);
-		
+		JLabel piece =  new JLabel(black);
+		piece.setPreferredSize(new Dimension(35, 35));
+		addConstraint(intersections[y][x], piece, 1, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
 	}
-	void printQuadrant(MouseEvent e) {
+	void printCoordinate(MouseEvent e) {
 		Component c = board.getComponentAt(new Point(e.getX(), e.getY()));
 		int mouseIndexX = (int)Math.floor(e.getX());
 		int mouseIndexY = (int)Math.floor(e.getY());
 		Dimension panelSize = c.getSize();
-		Point panelQuad = c.getLocation();
-		if(mouseIndexX < (panelSize.width/2) + panelQuad.x && mouseIndexY < (panelSize.height/2) + panelQuad.y) {
-			System.out.println("The mouse is in the first quadrant");
-		}
-		if(mouseIndexX > (panelSize.width/2) + panelQuad.x && mouseIndexY < (panelSize.height/2) + panelQuad.y) {
-			System.out.println("The mouse is in the second quadrant");
-		}
-		if(mouseIndexX < (panelSize.width/2) + panelQuad.x && mouseIndexY > (panelSize.height/2) + panelQuad.y) {
-			System.out.println("The mouse is in the third quadrant");
-		}
-		if(mouseIndexX > (panelSize.width/2) + panelQuad.x && mouseIndexY > (panelSize.height/2) + panelQuad.y) {
-			System.out.println("the mouse is in the fourth quadrant");
-		}
 		
 		timeline.append(((1 + (mouseIndexX+17)/panelSize.width)) + "," +(1 + (mouseIndexY+17)/panelSize.height) + "\n");
 		
